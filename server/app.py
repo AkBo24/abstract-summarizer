e29d98c715c2eb59ba04f3b9e248a9c1b6521e2e
Akshay Bodla <akshaybodla21@gmail.com>
2023-12-06 19:12:18 -0800

    Using requests from springer open api for now

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask_assets import Bundle, Environment
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from todo import todos
+from scholarly_util import query
 
 app = Flask(__name__)
 limiter = Limiter(
@@ -18,25 +19,31 @@ js = Bundle("src/*.js", output="dist/main.js") # new
 assets.register("css", css)
 css.build()
 
-
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
 @app.route("/search", methods=["POST"])
-@limiter.limit('1/minute')
+@limiter.limit('3/minute')
 def search_todo():
-    search_term = request.form.get("search")
+    search_query = request.form.get("search")
+    if not search_query:
+        return '<p>Empty search query</p>'
 
-    if not len(search_term):
-        return render_template("todo.html", todos=[])
+    query_res = query(search_query)
+    print(query_res)
+    return '<p>Mock endpoint</p>'
+    # search_term = request.form.get("search")
 
-    res_todos = []
-    for todo in todos:
-        if search_term in todo["title"]:
-            res_todos.append(todo)
+    # if not len(search_term):
+    #     return render_template("todo.html", todos=[])
 
-    return render_template("todo.html", todos=res_todos)
+    # res_todos = []
+    # for todo in todos:
+    #     if search_term in todo["title"]:
+    #         res_todos.append(todo)
+
+    # return render_template("todo.html", todos=res_todos)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ server/scholarly_util.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+from constants import API_KEY
+
+
+def query(search_query=''):
+    if len(search_query)==0:
+        return None
+
+    req_uri = f'http://api.springernature.com/metadata/pam?q=keyword:${search_query}&api_key={API_KEY}'
+    res = requests.get(req_uri)
+    return res
+
+# # Retrieve the author's data, fill-in, and print
+# search_query = scholarly..earch_pubs('Steven A Cholewiak')
+# author = scholarly.fill(next(search_query))
+# print(author)
+
+# # Print the titles of the author's publications
+# print([pub['bib']['title'] for pub in author['publications']])
+
+# # Take a closer look at the first publication
+# pub = scholarly.fill(author['publications'][0])
+# print(pub)
+
+# # Which papers cited that publication?
+# print([citation['bib']['title'] for citation in scholarly.citedby(pub)])
